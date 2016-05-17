20e908811189ebe424d1c87b20af5a7a28ddeff5
Skyside <616315821@qq.com>
2016-05-17 12:04:18 +0800

    index.php相关
    index.php主要结构已搭建好，数据库连接查询测试成功。

--- components/sidebar/sidebar.php
+++ components/sidebar/sidebar.php
@@ -3,27 +3,27 @@
 <head>
     <meta charset="UTF-8" />
     <title>Document</title>
-    <link rel="stylesheet" href="../../common/base.css" />
-    <link rel="stylesheet" href="sidebar.css" />
+    <link rel="stylesheet" href="/blog/common/base.css" />
+    <link rel="stylesheet" href="/blog/components/sidebar/sidebar.css">
 </head>
 <body>
     <div id="sidebar">
         <div id="head">
             <div id="avatar"></div>
-            <p>天边</p>
+            <p>天边的博客</p>
             <p id="size_small">Sing it for the people like us.</p>
         </div>
         <div id="body">
             <ul id="nav">
                 <li class="active"><a href="" ><i class="icon-home3"></i>首页</a></li>
                 <li>
-                    <a href="../../page/tech.php"><i class="icon-code"></i>技术<i class="icon-fold tech"></i></a>
+                    <a href="../../page/tech.php"><i class="icon-code"></i>技术<i class="icon-fold" data-type="tech"></i></a>
                 </li>
                     <ul id="tech" class="sublist hide">
                         <li><a href="../../page/html.php">Html</a></li>
                         <li><a href="../../page/css.php">Css</a></li>
                         <li><a href="../../page/javascript.php">JavaScript</a></li>
-                        <li><a href="../../page/others.php">其他</a></li>
+                        <li><a href="../../page/others.php">其他技术</a></li>
                     </ul>
                 <li>
                     <a href=""><i class="icon-life"></i>生活</a>
@@ -37,8 +37,8 @@
             </ul>
         </div>
     </div>
-    <script src="../../common/jquery.js"></script>
-    <script src="sidebar.js"></script>
+    <script src="/blog/common/jquery.js"></script>
+    <script src="/blog/components/sidebar/sidebar.js"></script>
 </body>
 
 </html>
