036fc49e3d5a6cb072be19cb795229efc104c6e8
liuMinXin@nongfadai.com <liuMinXin@nongfadai.com>
2018-01-02 16:41:16 +0800

    去掉cnzz站长统计

--- index.php
+++ index.php
@@ -34,6 +34,5 @@ show_classify();
 	</div>
 	<script src="/blog/common/jquery.js"></script>
 	<script src="/blog/index.js"></script>
-	<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1260446206'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s11.cnzz.com/z_stat.php%3Fid%3D1260446206' type='text/javascript'%3E%3C/script%3E"));</script>
 </body>
 </html>
